3f8068a07e2ec49bcfcb7ab0c2f0a907081678e4
Kristhian <109578339+kris11mm@users.noreply.github.com>
2025-11-05 17:19:32 -0500

    First Commit to repository InClass_Project

---
+++ InClass_Project/src/data_process.py
@@ -0,0 +1,28 @@
+class DataProcess:
+    def __init__():
+
+        return 0
+
+    def parseResume():
+
+        return 0
+
+    def parseJson():
+
+        return 0
+
+    def matchScore():
+
+        return 0
+
+    def relevanceScore():
+
+        return 0
+
+    def match():
+
+        return 0
+    
+    def exit():
+
+        exit(0)
