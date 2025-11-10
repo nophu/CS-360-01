993b66e34e719022e2b24ebaf558da08f9e35fdc
nophu <phuonc01@pfw.edu>
2025-11-10 17:42:14 -0500

    yurr

--- API.py
+++
@@ -1,4 +0,0 @@
-class API:
-    def __init__(self): return 0
-    def call(self): return 0
-    def exit(self): exit(0)
--- UserInterface.py
+++
@@ -1,5 +0,0 @@
-class UserInterface:
-    def __init__(self): return 0
-    def back(self): return 0
-    def expand(self): return 0
-    def exit(self):  exit(0)
