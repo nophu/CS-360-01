02f9a39542f1870bbf3906e2314a6ace737b5d24
nophu <phuonc01@pfw.edu>
2025-11-12 14:29:36 -0500

    yurr

--- AI Resume Analyzer/src/joblist_element.py
+++ AI Resume Analyzer/src/joblist_element.py
@@ -1,20 +1,18 @@
 class JobListElement:
-     def __init__(self, ):
-
-        return 0
+     def __init__(self,title,url, requirements,tags ):
+        self.title = title
+        self.url = url
+        self.requirements = requirements
+        self.tags = tags
      
-     def httpUrl(self):
-        return self.url
+     def httpUrl(self): return self.url
      
-     def requirements(self):
-        return self.requirements
+     def requirements(self): return self.requirements
      
-     def tags(self):
-        return self.tags
+     def tags(self): return self.tags
 
      def showInfo(self):
-        print()
-        print()
-        print()
-        print()
-        print()
+        print(f"Job Title: {self.title}")
+        print(f"Job URL: {self.url}")
+        print(f"Job Requirements: {self.requirements}")
+        print(f"Job Tags: {self.tags}")
