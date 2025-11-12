1e5933117e6661029fd11f1eca4a1121d5fe68dd
nophu <phuonc01@pfw.edu>
2025-11-12 14:45:22 -0500

    yurr

--- AI Resume Analyzer/src/joblist_element.py
+++ AI Resume Analyzer/src/joblist_element.py
@@ -1,21 +1,29 @@
-import nltk
-nltk.download('stopwords')
-
 class JobListElement:
-     def __init__(self,title,url, requirements,tags ):
+
+     def __init__(self, title, url, company, description, requirements = None, tags = None ):
         self.title = title
         self.url = url
-        self.requirements = requirements
-        self.tags = tags
+        self.company = company
+        self.description = description
+        self.requirements = requirements if requirements else []
+        self.tags = tags if tags else []
      
-     def httpUrl(self): return self.url
+     def get_url(self):
+         return self.url
      
-     def requirements(self): return self.requirements
+     def get_requirements(self):
+         return self.requirements
      
-     def tags(self): return self.tags
+     def get_tags(self):
+         return self.tags
 
-     def showInfo(self):
-        print(f"Job Title: {self.title}")
-        print(f"Job URL: {self.url}")
-        print(f"Job Requirements: {self.requirements}")
-        print(f"Job Tags: {self.tags}")
+     def to_dict(self):
+        return {
+            "title": self.title,
+            "url": self.url,
+            "company": self.company,
+            "description": self.description,
+            "requirements": self.requirements,
+            "tags": self.tags,
+        }
+
