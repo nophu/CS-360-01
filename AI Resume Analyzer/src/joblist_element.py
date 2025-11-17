0f0163d9b3dee91a1ec3e1f0c2dc6091740a5e1b
nophu <phuonc01@pfw.edu>
2025-11-17 17:02:47 -0500

    yurr

--- AI Resume Analyzer/src/joblist_element.py
+++ AI Resume Analyzer/src/joblist_element.py
@@ -1,4 +1,5 @@
 class JobListElement:
+
     # initialize basic job info
      def __init__(self, title, url, company, description, requirements = None, tags = None, rawDictionary = None):
 
@@ -16,25 +17,16 @@ class JobListElement:
         self.rawDictionary = rawDictionary if rawDictionary else {}
 
      # return job url
-     def get_url(self):
-         return self.url
+     def get_url(self): return self.url
 
      # return job requirement list
-     def get_requirements(self):
-         return self.requirements
+     def get_requirements(self): return self.requirements
 
      # return job tag list
-     def get_tags(self):
-         return self.tags
+     def get_tags(self): return self.tags
 
      # return job info as a dictionary
-     def to_dict(self):
-        return {
-            "title": self.title,
-            "url": self.url,
-            "company": self.company,
-            "description": self.description,
-            "requirements": self.requirements,
-            "tags": self.tags,
+     def to_dict(self): return {
+         "title": self.title, "url": self.url, "company": self.company, "description": self.description, "requirements": self.requirements, "tags": self.tags,
         }
 
