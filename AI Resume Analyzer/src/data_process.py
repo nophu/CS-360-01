fa2a7613162147ffc262df73b47e01c45f6618c6
Dawson Novotny <novodk01@pfw.edu>
2025-11-20 15:19:36 -0500

    update

--- AI Resume Analyzer/src/data_process.py
+++ AI Resume Analyzer/src/data_process.py
@@ -1,4 +1,3 @@
-import fileinput
 import json
 import joblist_element
 class data_process:
@@ -9,6 +8,15 @@ class data_process:
         self.jfilename = ""
         self.joblist = []
 
+    def create_joblist_element(self, title, url, company, description, requirements, tags): #new method for creating joblist_element
+        i = 0
+        for item in url:             #creates all the joblist_elements using the six lists based on the number of urls 
+            temp = joblist_element(title[i], item, company[i], description[i], requirements[i], tags[i])
+            self.joblist.append(temp)
+            i += 1
+
+
+        return self.joblist
 
     def parse_resume(self):
 
@@ -27,100 +35,97 @@ class data_process:
         self.jfilename.close()
 
         return self.jfilename
-
-    def parse_json(self, apiFile):
-        title = []                #these six lists are for storing elements for the four fields in joblist_element
-        url = []
-        company = []
-        description = []
-        requirments = []
-        tags = []
-        i = 0                     #used to iterate through title, requirments and tags
-
-        with open(apiFile, "r") as f:   #parses the json file from the api
+    
+    def parse_json(self, file): #parses a json to a dictionary
+         with open(file, "r") as f:  
             data = json.load(f)
 
-        for key, value in data:   #fills the six lists with the data from the json file
-            if key == "job_title" :
-                title.append(value)
-            elif key == "job_apply_link" :
-                url.append(value)
-            elif key == "employer_name" :
-                company.append(value)
-            elif key == "job_description" :
-                description.append(value)
-            elif key == "Qualifications" :
-                requirments.append(value)
-            elif key == "Responsibilities" :
-                tags.append(value)
+            return data
 
-        for item in url:             #creates all the joblist_elements using the six lists based on the number of urls 
-            temp = joblist_element(title[i], item, company[i], description[i], requirments[i], tags[i])
-            self.joblist.append(temp)
-            i += 1
+    def parse_Job_List(self, data):
+        
+        joblist = []
 
+        for _, job in data:
+            joblist.append(
+                joblist_element(
+                    job["job_title"],
+                    job["job_apply_link"],
+                    job["employer_name"],
+                    job["job_description"],
+                    job["job_highlights"]["Qualifications"],
+                    job["job_highlights"]["Responsibilities"]
+                )
+            )
 
-        return self.joblist
+        return joblist
+    
+   
 
     def match_score(self, job: joblist_element): #takes a joblist_element and creates a match score for it
         match = 0
-        requirments_number = 0
-        requirments = job.JobListElement.get_requirements
+        requirements_number = 0
+        requirements = job.JobListElement.get_requirements
         skills = []
 
-        with open(self.jfilename, "r") as f:  #this bit gets the skills from the json file of the users resume
-            data = json.load(f)
-        for key, value in data:   
-            if key == "skills" :
+        data = self.parse_json(self, self.jfilename)  #this bit gets the data from the json file of the users resume
+        
+        for key, value in data:   #gets user skills
+            if key == "skills" or key == "Skills":
                 skills.append(value)
 
-        for item in requirments: #checks if each requirment is in the user's listed skills
+        for item in requirements: #checks if each requirement is in the user's listed skills
             for element in skills:
-                if item == element:
+                if item.lower == element.lower:
                     match += 1
-            requirments_number += 1
+            requirements_number += 1
 
 
-        return (match / requirments_number) * 100 #returns match percentage
+        return (match / requirements_number) * 100 #returns match percentage
 
     def relevance_score(self, job: joblist_element): #returns relevance score for a give joblist_element as a percentage
-        related = 0
+        related_user_information = 0
         job_information = 0
         job_data = job.JobListElement.to_dict #gets job information to compare
-        with open(self.jfilename, "r") as f: #gets user resume info from the json file
-            data = json.load(f)
+        
+        data = self.parse_json(self, self.jfilename) #gets user resume information
         
         for key, value in job_data: #checks how much the job qualities match with the user's resume
             if key == "description" or key == "requirements" or key == "tags":
                 for tag, information in data:
                     if value == information:
-                        related += 1
+                        related_user_information += 1
                 job_information += 1
                 
 
-        return (related % job_information) * 100 #the percentage of how related the user resume is with the job information
+        return (related_user_information / job_information) * 100 #the percentage of how related the user resume is with the job information
 
     def match(self, job: joblist_element): #returns a list of skills matched and a list of missing skills
         matched = []
         missing = []
         found = False
         skills = []   
-        requirments = job.JobListElement.get_requirements
+        requirements = job.JobListElement.get_requirements
         
-        with open(self.jfilename, "r") as f:  #this bit gets the skills from the json file of the users resume
-            data = json.load(f)
-        for key, value in data:   
-            if key == "skills" : skills.append(value)
+        data = self.parse_json(self, self.jfilename)
 
-        for item in requirments: #checks if each requirment is in the user's listed skills or not and adds to the two lists accordingly
+        for key, value in data:   
+            if key == "skills" or key == "Skills":
+                skills.append(value)
+
+        for item in requirements: #checks if each requirement is in the user's listed skills or not and adds to the two lists accordingly
             for element in skills:
-                if item == element:
+                if item.lower == element.lower:
                     matched.append(element)
                     found = True
                     break
             if found == False:
                 missing.append(item)
             found = False
+            
+        
         return matched, missing
     
-    def exit(): exit(0)
+    def exit():
+
+        exit(0)
