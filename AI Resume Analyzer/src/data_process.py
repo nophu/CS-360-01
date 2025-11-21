f1caef083fea480b747aa79b11930de6c86fe16a
Alden <73655241+xXR3DXx@users.noreply.github.com>
2025-11-20 21:29:13 -0500

    Update data_process.py

--- AI Resume Analyzer/src/data_process.py
+++ AI Resume Analyzer/src/data_process.py
@@ -1,33 +1,26 @@
-import json
-import joblist_element
 class data_process:
-
-
     def __init__(self, file):
         self.filename = file
         self.jfilename = ""
         self.joblist = []
 
     def create_joblist_element(self, title, url, company, description, requirements, tags): #new method for creating joblist_element
+        import joblist_element
         i = 0
         for item in url:             #creates all the joblist_elements using the six lists based on the number of urls 
             temp = joblist_element(title[i], item, company[i], description[i], requirements[i], tags[i])
             self.joblist.append(temp)
             i += 1
 
-
         return self.joblist
 
     def parse_resume(self):
-
+        import json
         dict1 = {}
 
         with open(self.filename) as fh:
-
             for line in fh:
-                
                 command, description = line.strip().split(None, 1)
-
                 dict1[command] = description.strip()
 
         self.jfilename = open("resume.json", "w")
@@ -37,13 +30,14 @@ class data_process:
         return self.jfilename
     
     def parse_json(self, file): #parses a json to a dictionary
+         import json
+
          with open(file, "r") as f:  
             data = json.load(f)
-
             return data
 
     def parse_Job_List(self, data):
-        
+        import joblist_element
         joblist = []
 
         for _, job in data:
@@ -59,13 +53,12 @@ class data_process:
             )
 
         return joblist
-    
-   
 
-    def match_score(self, job: joblist_element): #takes a joblist_element and creates a match score for it
+    def match_score(self, job): #takes a joblist_element and creates a match score for it
+        import joblist_element
         match = 0
         requirements_number = 0
-        requirements = job.JobListElement.get_requirements
+        requirements = job.JobListElement.get_requirements()
         skills = []
 
         data = self.parse_json(self, self.jfilename)  #this bit gets the data from the json file of the users resume
@@ -76,37 +69,37 @@ class data_process:
 
         for item in requirements: #checks if each requirement is in the user's listed skills
             for element in skills:
-                if item.lower == element.lower:
+                if item.lower() == element.lower():
                     match += 1
             requirements_number += 1
 
+        return (match / requirements_number) #returns match percentage
 
-        return (match / requirements_number) * 100 #returns match percentage
-
-    def relevance_score(self, job: joblist_element): #returns relevance score for a give joblist_element as a percentage
+    def relevance_score(self, job): #returns relevance score for a give joblist_element as a percentage
+        import joblist_element
         related_user_information = 0
         job_information = 0
         job_data = job.JobListElement.to_dict #gets job information to compare
         
         data = self.parse_json(self, self.jfilename) #gets user resume information
         
-        for key, value in job_data: #checks how much the job qualities match with the user's resume
+        for key, value in job_data.items(): #checks how much the job qualities match with the user's resume
             if key == "description" or key == "requirements" or key == "tags":
                 for tag, information in data:
                     if value == information:
                         related_user_information += 1
                 job_information += 1
                 
-
         return (related_user_information / job_information) * 100 #the percentage of how related the user resume is with the job information
 
-    def match(self, job: joblist_element): #returns a list of skills matched and a list of missing skills
+    def match(self, job): #returns a list of skills matched and a list of missing skills
+        import joblist_element
         matched = []
         missing = []
+        skills = []  
         found = False
-        skills = []   
-        requirements = job.JobListElement.get_requirements
-        
+
+        requirements = job.JobListElement.get_requirements()
         data = self.parse_json(self, self.jfilename)
 
         for key, value in data:   
@@ -115,17 +108,15 @@ class data_process:
 
         for item in requirements: #checks if each requirement is in the user's listed skills or not and adds to the two lists accordingly
             for element in skills:
-                if item.lower == element.lower:
+                if item.lower() == element.lower():
                     matched.append(element)
                     found = True
                     break
             if found == False:
                 missing.append(item)
             found = False
-            
         
         return matched, missing
     
     def exit():
-
         exit(0)
